85ee805bd95b21220a8517af8454316688dedea7
Fatih Kilit <fatih@Fatih-MacBook-Air.local>
2022-07-11 09:54:06 +0300

    seaching is now diacritic intensitive

--- Photo Gallery/Core/Search/SearchViewModel.swift
+++ Photo Gallery/Core/Search/SearchViewModel.swift
@@ -14,8 +14,8 @@ class SearchViewModel: ObservableObject {
     private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
     let searchService: SearchService = SearchService()
     
-//    private let turkishChars: [Character] = ["ı", "ğ", "ç", "ş", "ö", "ü"]
-//    private let englishChars: [Character] = ["i", "g", "c", "s", "o", "u"]
+    private let turkishChars: [Character] = ["ı", "ğ", "ç", "ş", "ö", "ü"]
+    private let englishChars: [Character] = ["i", "g", "c", "s", "o", "u"]
     
     init() {
         addSubscribers()
@@ -27,10 +27,10 @@ class SearchViewModel: ObservableObject {
             .sink { [weak self] returnedSearchText in
                 self?.photos.removeAll()
                 
-                let text = returnedSearchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: "")
+                let text = returnedSearchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: "").folding(options: .diacriticInsensitive, locale: .current)
                 
                 self?.searchService.searchText = text
-                print(returnedSearchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: ""))
+                print(text)
                 self?.searchService.downloadSearchResult()
             }
             .store(in: &cancellables)
